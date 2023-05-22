5b3c50919fc67c58a0cb78840cb95bbaf8ddc5e2
kakandemanwell <kakandemanwell@gmail.com>
2023-05-22 06:42:05 -0700

    reverted code

--- models/base_model.py
+++ models/base_model.py
@@ -5,19 +5,11 @@ from datetime import datetime
 
 class BaseModel:
     """base model for the hbnb instance"""
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         """Initiates an instance of the basemodel class"""
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    if key in ['created_at', 'updated_at']:
-
-                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
         """returns a class of attributes"""
